3e74d57a5176e883e9b1299dc5291480ae531e81
Mostafa Kamel <mostafamt60@yahoo.com>
2025-03-02 14:19:54 +0200

    update gpt modal

--- src/components/Studio/StudioModals/StudioModals.jsx
+++ src/components/Studio/StudioModals/StudioModals.jsx
@@ -4,9 +4,10 @@ import Modal from "../../Modal/Modal";
 import SubObjectModal from "../../Modal/SubObjectModal/SubObjectModal";
 import { useStore } from "../../../store/store";
 import { default as BootstrapModal } from "react-bootstrap/Modal";
-import { Button, TextField } from "@mui/material";
+import { Button, CircularProgress, TextField } from "@mui/material";
 import axios from "../../../axios";
 import { createObjectGPT } from "../../../services/api";
+import { toast } from "react-toastify";
 
 const StudioModals = (props) => {
   const {
@@ -25,15 +26,27 @@ const StudioModals = (props) => {
 
   const [json, setJson] = React.useState("");
   const [result, setResult] = React.useState("");
+  const [loading, setLoading] = React.useState(false);
 
   const onClickSubmit = async (e) => {
+    setLoading(true);
     e.preventDefault();
-    const data = await createObjectGPT(JSON.parse(json));
-    console.log(data);
-    setResult(data.url);
-    setTimeout(() => {
-      window.open(data.url, "_blank").focus();
-    }, 2000);
+    let parsed = "";
+    try {
+      parsed = JSON.parse(json);
+    } catch (error) {
+      toast.error(String(error));
+      setLoading(false);
+      return;
+    }
+    const data = await createObjectGPT(parsed);
+    if (data) {
+      setResult(data?.url);
+      setTimeout(() => {
+        window.open(data.url, "_blank");
+      }, 500);
+    }
+    setLoading(false);
   };
 
   return (
@@ -65,7 +78,12 @@ const StudioModals = (props) => {
                 onChange={(e) => setJson(e.target.value)}
               />
               <div style={{ textAlign: "center" }}>
-                <Button variant="contained" type="submit">
+                <Button variant="contained" type="submit" disabled={loading}>
+                  {loading ? (
+                    <CircularProgress size="1rem" sx={{ mr: 2 }} />
+                  ) : (
+                    <></>
+                  )}
                   Submit
                 </Button>
               </div>
