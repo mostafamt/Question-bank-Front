7e55cae01dc48a14985759d79420ee076e5bba1b
mostafa <mostafa.kamel@almotahidaeducation.com>
2025-09-30 17:18:02 +0300

    add list to glossary and keywords

--- src/components/Tabs/GlossaryAndKeywords/GlossaryAndKeywords.jsx
+++ src/components/Tabs/GlossaryAndKeywords/GlossaryAndKeywords.jsx
@@ -1,9 +1,24 @@
 import React from "react";
 import { getTabObjects } from "../../../services/api";
 import { useQuery } from "@tanstack/react-query";
-import { CircularProgress } from "@mui/material";
+import { Box, CircularProgress } from "@mui/material";
+import List from "@mui/material/List";
+import ListItemButton from "@mui/material/ListItemButton";
+import ListItemIcon from "@mui/material/ListItemIcon";
+import ListItemText from "@mui/material/ListItemText";
+import Collapse from "@mui/material/Collapse";
+import ExpandLess from "@mui/icons-material/ExpandLess";
+import ExpandMore from "@mui/icons-material/ExpandMore";
 
 const GlossaryAndKeywords = (props) => {
+  const [open, setOpen] = React.useState([]);
+
+  const handleClick = (idx) => {
+    setOpen((prevState) =>
+      prevState.map((item, id) => (id === idx ? !item : item))
+    );
+  };
+
   const { chapterId } = props;
 
   const { data: tabObjects, isFetching } = useQuery({
@@ -12,17 +27,39 @@ const GlossaryAndKeywords = (props) => {
     refetchOnWindowFocus: false,
   });
 
+  React.useEffect(() => {
+    setOpen(Array(tabObjects?.length).fill(false));
+  }, [tabObjects]);
+
   if (isFetching) return <CircularProgress size="1rem" />;
 
   return (
     <div style={{ padding: "0.5rem" }}>
-      {tabObjects?.map((tabObject) => (
-        <p key={tabObject._id}>
-          <strong>{tabObject.term}</strong>
-          <br />
-          <span>{tabObject.definition}</span>
-        </p>
-      ))}
+      <List
+        sx={{ width: "100%", maxWidth: 360, bgcolor: "background.paper", p: 0 }}
+        component="div"
+      >
+        {tabObjects?.map((item, idx) => (
+          <div key={item._id}>
+            <ListItemButton onClick={() => handleClick(idx)} sx={{ p: 0 }}>
+              <ListItemIcon sx={{ minWidth: 0 }}>
+                {open[idx] ? <ExpandLess /> : <ExpandMore />}
+              </ListItemIcon>
+              <ListItemText
+                primary={item.term}
+                sx={{ fontWeight: "bold", "& > span": { fontWeight: "bold" } }}
+              />
+            </ListItemButton>
+            <Collapse in={open[idx]} timeout="auto" unmountOnExit>
+              <List component="div" disablePadding>
+                <Box sx={{ pl: 1, mb: 1 }}>
+                  <div>{item.definition}</div>
+                </Box>
+              </List>
+            </Collapse>
+          </div>
+        ))}
+      </List>
     </div>
   );
 };
