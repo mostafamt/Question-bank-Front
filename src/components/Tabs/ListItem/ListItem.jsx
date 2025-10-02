6ef1372db70e20f75b080b3d92f5b824118b2923
Mostafa Kamel <mostafamt60@yahoo.com>
2025-10-02 18:06:12 +0300

    Implemented Up/Down buttons for objects

--- src/components/Tabs/ListItem/ListItem.jsx
+++ src/components/Tabs/ListItem/ListItem.jsx
@@ -1,6 +1,9 @@
 import React from "react";
 import PlayArrowIcon from "@mui/icons-material/PlayArrow";
 import DeleteIcon from "@mui/icons-material/Delete";
+import StraightIcon from "@mui/icons-material/Straight";
+import NorthIcon from "@mui/icons-material/North";
+import SouthIcon from "@mui/icons-material/South";
 import { IconButton } from "@mui/material";
 
 import styles from "./listItem.module.scss";
@@ -14,7 +17,17 @@ const ListItem = ({ item, onPlay, onDelete, reader }) => {
           <PlayArrowIcon />
         </IconButton>
       </span>
-      {!reader && (
+      <span className={styles["up-down"]}>
+        <IconButton onClick={() => {}}>
+          <NorthIcon />
+        </IconButton>
+        <IconButton onClick={() => {}}>
+          <SouthIcon />
+        </IconButton>
+      </span>
+      {reader ? (
+        <></>
+      ) : (
         <span>
           <IconButton onClick={() => onDelete(item._id)}>
             <DeleteIcon color="error" />
