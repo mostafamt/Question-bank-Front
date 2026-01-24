96bb6f240ab82979c685397a5742476577d27136
Mostafa Kamel <mostafamt60@yahoo.com>
2026-01-24 20:35:46 +0200

    add object id text field to Interactive object

--- src/components/DrawnUI/InteractiveObject/InteractiveObject.jsx
+++ src/components/DrawnUI/InteractiveObject/InteractiveObject.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 
 import styles from "./interactiveObject.module.scss";
-import { Controller } from "react-hook-form";
+import { Controller, useWatch } from "react-hook-form";
 import { Box, Button, TextField } from "@mui/material";
 import { fullTextTrim } from "../../../utils/data";
 import { Link } from "react-router-dom";
@@ -25,6 +25,13 @@ const InteractiveObject = (props) => {
 
   const [showModal, setShowModal] = React.useState(false);
   const [selectedRowId, setSelectedRowId] = React.useState("");
+  const [manualId, setManualId] = React.useState("");
+
+  const fieldValue = useWatch({ control, name });
+
+  React.useEffect(() => {
+    setManualId(fieldValue || "");
+  }, [fieldValue]);
 
   const onClickSelectFromLibrary = () => {
     setShowModal(true);
@@ -41,6 +48,16 @@ const InteractiveObject = (props) => {
 
   const onClickCancel = () => {
     onCloseModal();
+  };
+
+  const onApplyManualId = () => {
+    if (manualId.trim()) {
+      setValue(name, manualId.trim());
+    }
+  };
+
+  const onManualIdChange = (e) => {
+    setManualId(e.target.value);
   };
 
   const newLabel = fullTextTrim(label);
@@ -61,53 +78,71 @@ const InteractiveObject = (props) => {
           control={control}
           rules={{ required }}
           defaultValue="" // Set default value if needed
-          render={({ field }) => (
-            <Box>
+          render={({ field, fieldState }) => (
               <Box>
-                <Box className={styles.header}>
-                  <Box>
-                    <Link
-                      to="/add-question"
-                      target="_blank"
-                      rel="noopener noreferrer"
-                    >
-                      <Button variant="text">Add object</Button>
-                    </Link>
+                <Box>
+                  <Box className={styles.header}>
+                    <Box>
+                      <Link
+                        to="/add-question"
+                        target="_blank"
+                        rel="noopener noreferrer"
+                      >
+                        <Button variant="text">Add object</Button>
+                      </Link>
 
-                    <Button variant="text" onClick={onClickSelectFromLibrary}>
-                      Select From Library
+                      <Button variant="text" onClick={onClickSelectFromLibrary}>
+                        Select From Library
+                      </Button>
+                    </Box>
+
+                    {field.value && (
+                      <Link
+                        to={`/show/${field.value}`}
+                        target="_blank"
+                        rel="noopener noreferrer"
+                      >
+                        <Button
+                          sx={{
+                            textTransform: "none",
+                            outlined: "none",
+                            border: "none",
+                          }}
+                        >
+                          <span>Open in new tab</span>
+                          <OpenInNewIcon />
+                        </Button>
+                      </Link>
+                    )}
+                  </Box>
+                  <Box className={styles["manual-input-row"]}>
+                    <TextField
+                      value={manualId}
+                      onChange={onManualIdChange}
+                      label="Object ID"
+                      size="small"
+                      fullWidth
+                      error={!!fieldState.error}
+                      helperText={fieldState.error?.message || (fieldState.error ? "This field is required" : "")}
+                    />
+                    <Button
+                      variant="contained"
+                      onClick={onApplyManualId}
+                      disabled={!manualId.trim()}
+                    >
+                      Apply
                     </Button>
                   </Box>
-
-                  {field.value && (
-                    <Link
-                      to={`/show/${field.value}`}
-                      target="_blank"
-                      rel="noopener noreferrer"
-                    >
-                      <Button
-                        sx={{
-                          textTransform: "none",
-                          outlined: "none",
-                          border: "none",
-                        }}
-                      >
-                        <span>Open in new tab</span>
-                        <OpenInNewIcon />
-                      </Button>
-                    </Link>
-                  )}
+                  <TextField
+                    {...field}
+                    label={newLabel}
+                    type={type}
+                    fullWidth
+                    hidden
+                  />
                 </Box>
-                <TextField
-                  {...field}
-                  label={newLabel}
-                  type={type}
-                  fullWidth
-                  hidden
-                />
+                {field.value && <ObjectRenderer id={field.value} />}
               </Box>
-              {field.value && <ObjectRenderer id={field.value} />}
-            </Box>
           )}
         />
       </div>
