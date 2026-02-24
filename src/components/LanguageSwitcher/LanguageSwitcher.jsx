2033863a98bef86af22216df158b1f11bc090c16
Mostafa Kamel <mostafamt60@yahoo.com>
2026-02-24 22:18:51 +0200

    change flag of uk to be flag of usa

--- src/components/LanguageSwitcher/LanguageSwitcher.jsx
+++ src/components/LanguageSwitcher/LanguageSwitcher.jsx
@@ -4,16 +4,17 @@ import Box from "@mui/material/Box";
 import { useStore } from "../../store/store";
 
 const FlagEN = () => (
-  <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 60 30" width="24" height="12">
-    <clipPath id="s"><path d="M0,0 v30 h60 v-30 z"/></clipPath>
-    <clipPath id="t"><path d="M30,15 h30 v15 z v15 h-30 z h-30 v-15 z v-15 h30 z"/></clipPath>
-    <g clipPath="url(#s)">
-      <path d="M0,0 v30 h60 v-30 z" fill="#012169"/>
-      <path d="M0,0 L60,30 M60,0 L0,30" stroke="#fff" strokeWidth="6"/>
-      <path d="M0,0 L60,30 M60,0 L0,30" clipPath="url(#t)" stroke="#C8102E" strokeWidth="4"/>
-      <path d="M30,0 v30 M0,15 h60" stroke="#fff" strokeWidth="10"/>
-      <path d="M30,0 v30 M0,15 h60" stroke="#C8102E" strokeWidth="6"/>
-    </g>
+  <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 190 100" width="24" height="13">
+    {/* Red and white stripes */}
+    <rect width="190" height="100" fill="#B22234"/>
+    <rect y="7.69" width="190" height="7.69" fill="#fff"/>
+    <rect y="23.08" width="190" height="7.69" fill="#fff"/>
+    <rect y="38.46" width="190" height="7.69" fill="#fff"/>
+    <rect y="53.85" width="190" height="7.69" fill="#fff"/>
+    <rect y="69.23" width="190" height="7.69" fill="#fff"/>
+    <rect y="84.62" width="190" height="7.69" fill="#fff"/>
+    {/* Blue canton */}
+    <rect width="76" height="53.85" fill="#3C3B6E"/>
   </svg>
 );
 
